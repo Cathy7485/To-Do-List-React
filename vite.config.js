857674b80f62a497d0b543a6ba001d361ce042a0
Cathy L <cathy141153@gmail.com>
2025-01-03 15:56:16 +0800

    修改編譯路徑以及新增部屬指令

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 上線或開發中
+  base: process.env.NODE_ENV === 'production' ? '/To-Do-List-React/' : '/',
   plugins: [react()],
 })
